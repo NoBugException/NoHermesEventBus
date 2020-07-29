4922b3d951de3ab3a026e64f1cc1e6c88e34aa36
NobugException <804431438@qq.com>
2020-07-29 14:24:31 +0800

    封装

--- app/src/main/java/com/example/nohermeseventbus/MainActivity.java
+++ app/src/main/java/com/example/nohermeseventbus/MainActivity.java
@@ -11,6 +11,8 @@ import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
 
+import com.example.nohermeseventbus.hermes.IConnectCallback;
+import com.example.nohermeseventbus.hermes.NoHermes;
 import com.example.nohermeseventbus.message.EventMessage;
 import com.example.nohermeseventbus.message.IEventMessage;
 
@@ -21,33 +23,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-//            startForegroundService(intent);
-//        } else {
-//            startService(intent);
-//        }
+//        NoHermes.getDefault().register();
 
-        Intent intent = new Intent(MainActivity.this, MyService.class);
-        bindService(intent, new ServiceConnection() {
+        NoHermes.getDefault().connect(getApplicationContext(), MyService.class, new IConnectCallback() {
             @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-                Log.d("yunchong", "连接成功");
-                IEventMessage iEventMessage = IEventMessage.Stub.asInterface(service);
-                if(iEventMessage != null){
-                    try {
-                        EventMessage eventMessage = iEventMessage.getEventMessage();
-                        Log.d("yunchong", "从服务器取到的值："+eventMessage.getEventName());
-                    } catch (RemoteException e) {
-                        e.printStackTrace();
-                    }
-                }
+            public void connectSuccess() {
+                // 连接成功
+
             }
 
             @Override
-            public void onServiceDisconnected(ComponentName name) {
-                Log.d("yunchong", "连接失败");
+            public void connectFailed() {
+                // 连接失败
+
             }
-        }, Context.BIND_AUTO_CREATE);
-        startService(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/example/nohermeseventbus/hermes/IConnectCallback.java
@@ -0,0 +1,11 @@
+package com.example.nohermeseventbus.hermes;
+
+public interface IConnectCallback {
+
+    // 连接成功
+    void connectSuccess();
+
+    // 连接失败
+    void connectFailed();
+
+}
